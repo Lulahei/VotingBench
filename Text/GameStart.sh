d95484cead6b25d13aea51ef785445b7669d8634
lulahei <644019738@qq.com>
2026-01-19 12:19:04 +0800

    upgrade text mjl-2026-01-19

--- Text/GameStart.sh
+++ Text/GameStart.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-export gpt_apikey=sk-wtFIFjn8l4IsZceMGiTb7bs0nw12dZj9mdo14hFDdteaMAXU
+export gpt_apikey=
 export gpt_modelpath=gpt-4o
-export doubao_apikey=ea266663-488d-4cfd-8f0f-b08b008a450a
-export doubao_modelpath=ep-20241210150815-whlpl
-export kimi_apikey=sk-wEgIdguz6sW6S3mpVMjDocxrwJMPut6YnzOgMDx5RIJEHaYv
+export doubao_apikey=
+export doubao_modelpath=
+export kimi_apikey=
 export kimi_modelpath=moonshot-v1-8k
-export Qwen_apikey=sk-4cf3c52339a44171ba511a3ba6331e4d
+export Qwen_apikey=
 export Qwen_modelpath=qwen-plus
-#export glm_apikey=10859b619530cbc060694f97d830dcfe.5tWdciiUwYrPchcD
-export glm_apikey=fc72ff77ba19d5d6119be747a06865c3.ipHsgFIzvr0zth7b
+export glm_apikey=
 export glm_modelpath=glm-4-plus
-export ds_apikey=sk-9f37115545ab4814b79b7145152e80bd
+export ds_apikey=
 python gui2.py
 #nohup python game.py > EN_artical_first_0218.txt 2>&1 &
 #nohup python game_one_word.py > log1.txt 2>&1 &
